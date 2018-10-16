1448d758eb7172d751bceb75915786a2f30a9b0f
Predrika Br Ginting <ichapredrikagm@gmail.com>
2018-10-17 00:48:23 +0700

    logout

--- app/src/main/java/com/predrika/icha/autobiblio/MainActivity.java
+++ app/src/main/java/com/predrika/icha/autobiblio/MainActivity.java
@@ -1,10 +1,17 @@
 package com.predrika.icha.autobiblio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
 
     private String email;
+
+
 
     @Override
     public void onStart() {
@@ -28,7 +37,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser Users= FirebaseAuth.getInstance().getCurrentUser();
+
+        TextView welcomeTV= findViewById(R.id.welcomeText);
+        welcomeTV.setText("Hello "+ Users.getEmail());
+
+    }
+
+    public void logoutClick(View view){
+        try{
+            mAuth.signOut();
+            Toast.makeText(MainActivity.this, "Logout successful", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent( MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
